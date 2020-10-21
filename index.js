28f07c31a0276e2b65a10440e52e2041282a36b0
Alexandr <Akeley1994@gmail.com>
2020-10-21 12:16:48 +0300

    create server

---
+++ index.js
@@ -0,0 +1,18 @@
+const app = require('express')();
+
+const PORT = process.env.PORT || 5000;
+
+app.use('/public', (req, res) => {
+  res.json('I am public route!');
+});
+
+app.use('/private', (req, res) => {
+  if (!req.query.login || !req.query.password) {
+    return res.status(401).send('Unauthorized');
+  }
+  res.json(`Hi ${req.query.login}. I am a private route. I haven't been taught how to return a token yet, but my developer knows how. I believe him.`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Started on ${PORT}`)
+},)
